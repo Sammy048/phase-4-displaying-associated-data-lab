04c7650736c7b11340f0c7b8448ee309d865f536
sammy_masibo <sammymasib000@gmail.com>
2023-03-21 06:10:22 -0700

    Completed assignment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+    def show
+        users = User.find_by(id: params[:id])
+        if users
+          render json: users
+        else
+          render json: { error: "Validation errors" }, status: :not_found
+        end
+    end
 end
